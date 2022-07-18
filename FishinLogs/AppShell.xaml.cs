bc60b1ebf725181244e9ed0accf05508d4b2abbe
Imants Treidis <imants.treidis@gmail.com>
2022-07-18 15:02:29 +0300

    Initial commit

---
+++ FishinLogs/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FishinLogs;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ FishinLogs/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FishinLogs;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
