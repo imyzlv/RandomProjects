5b36fc3b77f8a70242cb6972308b2dddca17a84a
Imants Treidis <imants.treidis@gmail.com>
2022-07-21 12:36:03 +0300

    Calc working on iOS

--- MauiCalc/MainPage.xaml.cs
+++ MauiCalc/MainPage.xaml.cs
@@ -1,15 +1,65 @@
-﻿namespace MauiCalc;
+﻿using System;
+namespace MauiCalc;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	double firstNumber = 0, secondNumber = 0;
+	string operand = null;
+	bool operationalState = false;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	
+    void OnClear(System.Object sender, System.EventArgs e)
+    {
+		result.Text = "0";
+		firstNumber = 0;
+		secondNumber = 0;
+		operand = null;
+		operationalState = false;
+    }
+
+	void OnNumberClicked(System.Object sender, System.EventArgs e)
+    {
+		Button button = (Button)sender;
+		string btnText = button.Text;
+		if (result.Text == "0" || operationalState)
+		{
+			result.Text = btnText;
+		}
+		else
+		{
+			result.Text += btnText;
+		}
+    }
+
+	void OnOperandClicked(System.Object sender, System.EventArgs e)
+    {
+		//string result = null;
+		Button button = (Button)sender;
+		firstNumber = Double.Parse(result.Text);
+		operand = button.Text;
+		operationalState = true;
+		//result.Text = "0";
+    }
+
+	void OnCommaClicked(System.Object sender, System.EventArgs e)
+    {
+		if(!result.Text.Contains('.'))
+        {
+			result.Text += ".";
+        }
+    }
+
+	void OnResult(System.Object sender, System.EventArgs e)
+    {
+		Button button = (Button)sender;
+		secondNumber = Double.Parse(result.Text);
+		result.Text = Service.Calculation.PerformCalculation(firstNumber, secondNumber, operand); 
+    }
+
 }
 
 
---
+++ MauiCalc/Service/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MauiCalc.Service
+{
+    public class Calculation
+    {
+        public static string PerformCalculation(double firstNumber, double secondNumber, string operand)
+        {
+            string result = "Division with 0 is not allowed";
+            switch(operand)
+            {
+                case "/":
+                    if(secondNumber==0)
+                    {
+                        break;
+                    }
+                    result = (firstNumber / secondNumber).ToString();
+                    break;
+
+                case "x":
+                    result = (firstNumber * secondNumber).ToString();
+                    break;
+
+                case "-":
+                    result = (firstNumber - secondNumber).ToString();
+                    break;
+                case "+":
+                    result = (firstNumber - secondNumber).ToString();
+                    break;
+
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
